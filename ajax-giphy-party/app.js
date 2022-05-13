654f1675e3bf6bab478132e5d13a0288c2b40117
JB Markowicz <hunterhudson3@gmail.com>
2022-05-12 20:43:45 -0600

    Adjust alignment of search form, adjust appearance of submit button, add remove images button, add functionality to get images from GIPHY, add functionality to apply GIPHY image to img HTML element.

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,2 +1,21 @@
 console.log("Let's get this party started!");
 
+async function getImage(query) {
+    let res = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { api_key: 'PIPc1u6U9ooqgkM5pC1RX8M1tHel8fHb', q: query} });
+
+    return res;
+}
+
+function makeImage(url) {
+    // Source (https://stackoverflow.com/questions/8013792/how-to-create-a-new-img-tag-with-jquery-with-the-src-and-id-from-a-javascript-o, accessed 12 May 2022)
+    const $img = $(`<img src="${url}" alt="">`); 
+}
+
+$('#search-button').click(function(e) {
+    e.preventDefault(); 
+    let $query = $('#search').val();
+    let data = getImage($query);
+
+    console.log(data);
+});
+
